92737a0b19b7ef076563d2bdea44015678f6c923
Pomin Wu <pomin5@gmail.com>
2015-09-07 18:55:04 +0800

    prepare package for PyPI

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'moretext',
+    packages = ['moretext'],
+    version = '0.1',
+    description = 'Get dummy Chinese text (lorem ipsum) with Handlino serivce.',
+    author = 'Pomin Wu',
+    author_email = 'pomin5@gmail.com',
+    url = 'https://github.com/pm5/python-moretext',
+    keywords = ['test', 'lorem', 'ipsum', 'placeholder'],
+    classifiers = [],
+)
